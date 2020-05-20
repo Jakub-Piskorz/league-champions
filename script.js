ead72f696449ce464ce8c52428969590100ca2a0
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-05-20 23:39:04 +0200

    Design and API update

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-(async () => {
-  const response = await fetch(
-    "http://ddragon.leagueoflegends.com/cdn/10.10.3216176/data/en_US/champion.json"
-  );
-  const champs = await response.json();
-  let div = document.createElement("div");
-  div.innerHTML = JSON.stringify(champs);
-  document.body.appendChild(div);
-  console.log(document.body.firstChild.text);
-})();
+class leagueAPI {
+  constructor() {
+    this.json = null;
+    this.html = null;
+    this.list = null;
+    this.htmlRoot = null;
+    this.render = null;
+  }
+  _error(err) {
+    throw new Error(err);
+    return;
+  }
+  download() {
+    console.log("Initiating download...");
+    fetch(
+      "http://ddragon.leagueoflegends.com/cdn/10.10.3216176/data/pl_PL/champion.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .catch(this._error)
+      .then((data) => {
+        this.json = data;
+        this.list = Object.keys(data.data);
+        console.log("Download completed.");
+      })
+      .catch(this._error);
+  }
+  buildHtml(htmlRoot = this.htmlRoot) {
+    if (!htmlRoot || !this.json) {
+      console.error("Not enough data. Try using .download()");
+      return;
+    }
+  }
+}
 
-// const createChamp => {
-//     document.createElement("div", )
-// }
+const champs = new leagueAPI();
